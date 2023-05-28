794ac1eb5f66dab7a5dea3432f4c46e5371db734
dev <remnev.dev@gmail.com>
2023-05-28 22:23:55 +0300

    feat: add block main page

---
+++ components/blockPage/blockPage.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from './blockPage.module.css';
+import clsx from 'clsx';
+import block from 'module-clsx';
+const style = block(styles);
+import Image from 'next/image';
+import Button from '../UI/buttons';
+
+const BlockPage = ({ img, alt, title, subtitle, children, reverse, bgrFace, bgrProf }) => {
+  const blockReverse = clsx({ block__reverse: reverse });
+  const backgroundFace = clsx({ bgr__face: bgrFace });
+  const backgroundProf = clsx({ bgr__prof: bgrProf });
+
+  return (
+    <section className={`${styles.block} ${style(backgroundFace)} ${style(backgroundProf)}`}>
+      <div className={`${styles.block__inner} wrapper`}>
+        <Image src={img} alt={alt} rel="preload" />
+        <div className={`${styles.content} ${style(blockReverse)}`}>
+          <h2 className={styles.title}>{title}</h2>
+          <p className={styles.subtitle}>{subtitle}</p>
+          <div className={styles.content__buttons}>
+            <Button text="Scopri" />
+            <Button text="Scaricare" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default BlockPage;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,32 @@
 import styles from '../styles/Home.module.css';
 import HeaderContent from '../components/headerContant/headerContant';
+import BlockPage from '../components/blockPage/blockPage';
+
+import blockFace from '../public/blockFace.webp';
 
 export default function Home() {
   return (
+    <>
       <HeaderContent />
+      <section className={`${styles['about-text']} wrapper`}>
+        <h2 className={styles['about-text__heding']}>
+          LINDA KRISTEL <br />
+          Una storia di Bellezza
+        </h2>
+        <p className={styles['about-text__subheding']}>
+          Perché ogni persona ha la sua storia disegnata sulla propria pelle e non può un solo
+          trattamento essere uguale per tutti. Questo ha portato Linda Kristel ad essere
+          all’avanguardia nella ricerca, e a sviluppare sempre nuovi prodotti.
+        </p>
+      </section>
+
+      <BlockPage
+        img={blockFace}
+        bgrFace={true}
+        title="Linda Kristel per la cura del viso"
+        subtitle="Qualità, ricerca, avanguardia nella prevenzione e nei trattamenti antinvecchiamento; questi gli obiettivi degli specialisti dei laboratori Linda Kristel."
+        // reverse={true}
+      />
+    </>
   );
 }
